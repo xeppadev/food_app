60ef6ce48eaf4e60ffab13cf55c9f4680f1af6de
Cristhian Rodriguez <cprodriguez@unitru.edu.pe>
2024-09-12 17:22:46 -0500

    add env file

--- firebase-config.ts
+++ firebase-config.ts
@@ -3,12 +3,21 @@
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const apikey = process.env.EXPO_PUBLIC_API_KEY;
+const authDomain = process.env.EXPO_PUBLIC_AUTH_DOMAIN;
+const projectId = process.env.EXPO_PUBLIC_PROJECT_ID;
+const storageBucket = process.env.EXPO_PUBLIC_STORAGE_BUCKET;
+const messagingSenderId = process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID;
+const appId = process.env.EXPO_PUBLIC_APP_ID;
+const measurementId = process.env.EXPO_PUBLIC_MEASUREMENT_ID;
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyBPqgNIcO_RbQJLPXKPSlCcLHv-dDuCXNk",
-  authDomain: "app-food-cfb81.firebaseapp.com",
-  projectId: "app-food-cfb81",
-  storageBucket: "app-food-cfb81.appspot.com",
-  messagingSenderId: "48876233634",
-  appId: "1:48876233634:web:75a22bfdf54cd817ddc8fb",
-  measurementId: "G-MECLZP1WKJ",
+  apiKey: apikey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+  measurementId: measurementId,
 };
